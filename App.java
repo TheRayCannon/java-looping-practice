965d40ca62f6b1dcb23ef9d021fee45ebfad9838
Shawn Cannon <scanno16@ford.com>
2022-05-24 11:44:48 -0600

    complete your looloop Dolories

--- App.java
+++ App.java
@@ -1,14 +1,31 @@
 class App {
-  public static void main(String args[]){
+  public static void main(String args[]) {
     App.forLoop();
     App.whileLoop();
     App.doWhileLoop();
   }
 
+  static String message = "I love java";
+
   private static void forLoop() {
+    for (int foo = 0; foo < 5; foo++) {
+      System.out.println("using for loop: " + message);
+    }
   }
+
   private static void whileLoop() {
+    int i = 0;
+    while (i < 5) {
+      System.out.println("using while loop " + message);
+      i++;
+    }
   }
+
   private static void doWhileLoop() {
+    int i = 0;
+    do {
+      System.out.println("using do while : " + message);
+      i++;
+    } while (i < 5);
   }
 }
